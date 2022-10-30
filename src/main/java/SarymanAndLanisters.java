fbf1129f07df523bebe955a0c9a038d408dc97ec
Ilya Danilov <esozap32@mail.ru>
2022-10-30 22:10:24 +0400

    SarymanAndLanisters

---
+++ src/main/java/SarymanAndLanisters.java
@@ -0,0 +1,9 @@
+public class SarymanAndLanisters {
+    public static void main(String[] args) {
+        int humansoldier = 500;
+        int orcsoldier = 650;
+        int soldierunion = humansoldier + orcsoldier;
+        System.out.println("Союз войск: " + soldierunion + " единиц войск.");
+
+    }
+}
